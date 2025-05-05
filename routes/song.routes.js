e48df247e2701098c726b548f0e60b7197a0b2ab
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-05-05 07:14:44 -0400

    Add file validation middleware to song creation route

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -1,10 +1,11 @@
 module.exports = app => {
+    let validateFile = require("../middlewares/fileValidation.js");
     let router = require("express").Router();
     const controller = require("../controllers/song.controller.js");
 
     router.get("/", controller.getSongList);
     router.get("/:id", controller.getSongById);
-    router.post("/", controller.postSongCreate);
+    router.post("/", validateFile("file"), controller.postSongCreate);
     router.put("/:id", controller.putSongUpdate);
     router.patch("/:id", controller.patchSongUpdate);
     router.delete("/:id", controller.deleteSong);
