12209665a00e6d54a9c8595a5ce7021dc67762c1
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-05-04 21:03:17 -0400

    Initialize Express server with CORS, body parsing, file upload, and static file serving

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require('express-fileupload');
+var cors = require('cors')
+
+const app = express();
+const port = 3000;
+const db = require("./models/");
+
+//cors
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 50 * 1024 * 1024 },
+    abortOnLimit: true,
+}));
+
+// serve static files
+app.use('/uploads', express.static('uploads'));
+
+db.sequelize.sync({
+    // force: true // drop tables and recreate
+}).then(() => {
+    console.log("db resync");
+});
+
+require("./routes")(app);
+app.listen(port, () => {
+    console.log(`Spotify app listening on port ${port}`);
+});
